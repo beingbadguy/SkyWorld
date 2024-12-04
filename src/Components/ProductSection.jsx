3d5653edec3401897f4f73b1da88b1475a20f93c
Aman Kumar <authorisedaman@gmail.com>
2024-12-04 19:29:23 +0530

    added

--- src/Components/ProductSection.jsx
+++ src/Components/ProductSection.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextStore } from "../Store/StoreContext";
+import { IoStarSharp } from "react-icons/io5";
 
 const ProductSection = () => {
+  const { data } = useContext(ContextStore);
   const products = [
     {
       id: 1,
@@ -58,25 +61,27 @@ const ProductSection = () => {
     <div className=" px-4 md:px-12 py-6">
       <h2 className="text-3xl font-bold  mb-8">Shop by Products</h2>
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center">
-        {products.map((p, index) => (
+        {data.map((p, index) => (
           <div
             key={index}
             className="relative flex items-center justify-center flex-col gap-4 bg-gray-100 p-7 rounded"
           >
-            <div className="absolute top-0 right-0 p-2 text-pink-400">
-              {p.tag}
-            </div>
             <div className="w-36 h-36 sm:h-48 sm:w-48 md:h-54 md:w-54  flex items-center justify-center rounded-full  overflow-hidden">
-              <img src={p.image} alt="" />
+              <img src={p.img} alt="" />
             </div>
             <div className="flex flex-col items-center">
-              <p className="md:text-xl font-semibold text-gray-700">{p.name}</p>
-              <div className="flex gap-2">
-                <p className="text-sm font-semibold text-pink-400">
+              <p className="md:text-xl  text-gray-700">{p.name}</p>
+              {/* <div>
+                {Array.from({ length: p.rating }, (_, index) => (
+                  <IoStarSharp key={index} className="text-pink-400" />
+                ))}
+              </div> */}
+              <div className="flex gap-2 mt-2">
+                <p className="text-sm font-semibold text-pink-400 ">
                   ₹{p.price}
                 </p>
                 <p className="text-sm font-semibold text-gray-400 line-through">
-                  ₹{p.originalPrice}
+                  ₹{p.original_price}
                 </p>
               </div>
             </div>
--- src/Store/StoreContext.jsx
+++ src/Store/StoreContext.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+import products from "../data/data.json";
 
 export const ContextStore = createContext();
 
 const ContextProvider = ({ children }) => {
-  const [data, setData] = useState("Namaste Ronaldo");
+  const [data, setData] = useState(products);
   return (
     <ContextStore.Provider value={{ data }}>{children}</ContextStore.Provider>
   );
